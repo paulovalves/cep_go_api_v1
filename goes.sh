7cf3fdfa6e2e578135cb40f6187f408c97440836
paulovalves <paulovalves@outlook.com>
2024-03-31 21:37:47 -0300

    initial commit

---
+++ goes.sh
@@ -0,0 +1,10 @@
+!#/bin/bash sh
+go get github.com/gin-gonic/gin
+go get gorm.io/gorm
+go get github.com/lib/pq
+go get -u github.com/jinzhu/gorm/dialects/postgres
+go get github.com/dgrijalva/jwt-go
+go get github.com/google/uuid
+go get github.com/cloudinary/cloudinary-go/v2
+go get github.com/spf13/viper
+go get -u -v github.com/gorilla/mux
