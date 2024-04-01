36da44ab05cedd3aca6ac28b1582d90eaf93287f
paulovalves <paulovalves@outlook.com>
2024-03-31 22:25:28 -0300

    create and start a new server

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	entity "models/entity"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := newServer()
+	server.run(":8080")
+}
+
+func newServer() *entity.Server {
+	return &entity.Server{
+		router: gin.Default(),
+	}
+}
+
+func (s *entity.Server) run(addr) {
+	r := s.router.Run(addr)
+	if r != nil {
+		log.Fatalf("Error while running server: %v", r)
+	}
 }
