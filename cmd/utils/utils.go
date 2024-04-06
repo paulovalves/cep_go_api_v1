85a5c2b76526b67212e54246617346ae90a8641f
paulovalves <paulovalves@outlook.com>
2024-04-06 07:13:08 -0300

    not working function to check if the added category is valid

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -1,8 +1,22 @@
 package utils
 
-import "github.com/google/uuid"
+import (
+	"strings"
+
+	"github.com/google/uuid"
+	entity "models/entity"
+)
 
 func IsValidUUID(uuidStr string) bool {
 	_, err := uuid.Parse(uuidStr)
 	return err == nil
+}
+
+func ValidateCategory(category entity.Category) bool {
+	if strings.TrimRight(category.Name, "\n") == "" ||
+		strings.TrimRight(category.Status, "\n") == "" {
+		return false
+	}
+
+	return true
 }
