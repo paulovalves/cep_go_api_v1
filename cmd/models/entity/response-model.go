238fac64855032c605962f7cfcc2480f5f8a46b3
paulovalves <paulovalves@outlook.com>
2024-04-07 22:44:02 -0300

    added made up constructor to response model

--- cmd/models/entity/response-model.go
+++ cmd/models/entity/response-model.go
@@ -1,7 +1,14 @@
 package models
 
 type ResponseModel struct {
-	data    interface{}
-	err     error
-	message string
+	Data    interface{}
+	Error   error
+	Message string
+}
+
+func SetResponse(data interface{}, err error, message string) ResponseModel {
+	if err != nil {
+		return ResponseModel{Data: nil, Error: err, Message: message}
+	}
+	return ResponseModel{Data: data, Error: nil, Message: message}
 }
