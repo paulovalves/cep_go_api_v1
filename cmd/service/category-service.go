f63630e1da3967ff635581406f08ba1b92211768
paulovalves <paulovalves@outlook.com>
2024-04-07 22:55:31 -0300

    refactor category service response

--- cmd/service/category-service.go
+++ cmd/service/category-service.go
@@ -3,9 +3,10 @@ package service
 import (
 	"log"
 
+	entity "models/entity"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	entity "models/entity"
 )
 
 var (
@@ -17,72 +18,66 @@ func SetDB(db *gorm.DB) {
 	DB = db
 }
 
-func GetCategories() ([]entity.Category, error) {
+func GetCategories() entity.ResponseModel {
 	var data []entity.Category
 
 	err := DB.Table(table).Find(&data).Error
 	if err != nil {
 		log.Fatalf("Error while fetching categories: %v", err)
-		return nil, err
+		return entity.SetResponse(nil, err, "Error while fetching categories")
 	}
 
-	return data, nil
+	return entity.SetResponse(data, nil, "success")
 }
 
-func GetCategoryById(id string) (entity.Category, error) {
+func GetCategoryById(id string) entity.ResponseModel {
 	var data entity.Category
 
 	if err := DB.Table(table).Where("id = ?", id).Find(&data).Error; err != nil {
 		log.Fatalf("Error while fetching category: %v", err)
-		return entity.Category{}, err
+		return entity.SetResponse(nil, err, "Error while fetching category")
 	}
-
-	return data, nil
+	return entity.SetResponse(data, nil, "success")
 }
 
-func GetCategoriesByStatus(status string) ([]entity.Category, error) {
+func GetCategoriesByStatus(status string) entity.ResponseModel {
 	var data []entity.Category
 
 	if err := DB.Table(table).Where("status = ?", status).Find(&data).Error; err != nil {
 		log.Fatalf("Error while fetching categories: %v", err)
-		return nil, err
+		return entity.SetResponse(nil, err, "Error while fetching categories")
 	}
 
-	return data, nil
+	return entity.SetResponse(data, nil, "success")
 }
 
-func CreateCategory(category entity.Category) (entity.Category, error) {
-	log.Printf("Category: %v", category)
-	// if utils.ValidateCategory(category) {
-	// 	log.Fatalf("Category is not valid %v", category)
-	// 	return entity.Category{}, errors.New("Category is not valid")
-	// }
-	if err := DB.Table(table).Create(&category).Error; err != nil {
+func CreateCategory(data entity.Category) entity.ResponseModel {
+	if err := DB.Table(table).Create(&data).Error; err != nil {
 		log.Fatalf("Error while creating category: %v", err)
-		return category, err
+		return entity.SetResponse(nil, err, "Error while creating category")
 	}
 
-	return category, nil
+	return entity.SetResponse(data, nil, "success")
 }
 
-func UpdateCategory(category entity.Category) (entity.Category, error) {
+func UpdateCategory(category entity.Category) entity.ResponseModel {
 	err := DB.Table(table).Where("id = ?", category.Id).Update(map[string]interface{}{
 		"name":   category.Name,
 		"status": category.Status,
 	}).Error
 	if err != nil {
 		log.Fatalf("Error while updating category: %v", err)
-		return category, err
+		return entity.SetResponse(category, err, "Error while updating category")
 	}
 
-	return category, nil
+	return entity.SetResponse(category, nil, "success")
 }
 
-func DeleteCategory(id string) (bool, error) {
+func DeleteCategory(id string) entity.ResponseModel {
 	if err := DB.Table(table).Delete(id).Error; err != nil {
 		log.Fatalf("Error while deleting category: %v", err)
-		return false, err
+		return entity.SetResponse(false, err, "Error while deleting category")
 	}
 
-	return true, nil
+	return entity.SetResponse(true, nil, "success")
 }
